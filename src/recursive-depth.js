b73fc3667ad849ed8909a16b5f27d4fdf0917fc1
aleks_pv <apanov08@gmail.com>
2022-04-29 22:22:58 +0400

    refactor: delete console.log() from recursive-depth solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -17,7 +17,6 @@ class DepthCalculator {
     let count = 1;
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
-        console.log(count);
         return count + this.calculateDepth(arr.flat());
       }
     }
